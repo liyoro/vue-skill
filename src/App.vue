d0459ffb682302f509cc9f510fbcea4461660fd2
liyoro@163.com <liyoro@163.com>
2021-08-13 23:16:02 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <router-link to="/">Home</router-link>
     <router-view />
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
     <div>
-      <router-link to="/">Home</router-link> |
       <router-link to="/mockdatatest">MockDataTest</router-link> |
       <router-link to="/charttest">chartTest</router-link> |
       <router-link to="/dynamictabletest">dynamicTableTest</router-link> |
