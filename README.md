723682a19206ea8b452585769349ddebd4749564
v-liyr <liyoro@163.com>
2022-03-11 16:47:23 +0800

    20200311

--- README.md
+++ README.md
@@ -22,3 +22,7 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## 环境
+
+平时是在`node`版本`v12.22.4`环境下运行`vue ui`开发的，`npm`版本是`6.14.14`，用的`nvm`管理版本切换，在别的版本应该也可以开发，自行尝试了。
--- src/views/somethingTest/index.js
+++ src/views/somethingTest/index.js
@@ -8,7 +8,8 @@ export default {
     return {
       showText: '',
       test1: {},
-      test2: []
+      test2: [],
+      test3: []
     }
   },
   mounted() {
@@ -22,6 +23,7 @@ export default {
       if (code !== 0) return
       this.test1 = result.test1
       this.test2 = result.test2
+      this.test3 = result.test3
 
       // 可枚举键值对的值
       // const values = Object.values(this.test1)
@@ -30,11 +32,26 @@ export default {
       // const entries = Object.entries(this.test1)
 
       // 异步迭代，不能保证马上获取到数据的情景，保证是顺序执行
-      for await (const item of this.test2) {
-        console.log(111, item)
-      }
+      // for await (const item of this.test2) {
+      //   console.log(111, item)
+      // }
 
-      // this.showText = entries
+      // 数组降维，默认1，默认去除空项（空值不去除，例如: ''、 ""）
+      // const flatRes = this.test3.flat()
+      // 数组嵌套3层了
+      // const flatRes = this.test3.flat(2)
+      // 无穷降维，多维-->一维，可不关注数组嵌套多少层了
+      // const flatRes = this.test3.flat(Infinity)
+
+      // includes
+      // const includesRes = this.test3.includes(3)
+
+      // 空值合并运算符
+      // const a = 0
+      // const b = a ?? 2
+
+      // globalThis
+      // this.showText = b
     }
   },
   watch: {}
